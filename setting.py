14fff0515b43f1beb01ee1652e7bc6e3f6c70213
Lia Leo <lialeo@LiadeMacBook-Pro.local>
2020-07-06 16:35:49 +0800

    update

--- setting.py
+++ setting.py
@@ -1,9 +1,9 @@
-db_password = "123456789"
-db_user = "root"
+# db_password = "123456789"
+# db_user = "root"
 
 
 
-# db_password = "kVXBUbyI6IjIwMjAtMDgtMDIifSx7ImNvZGUiOiJQREIiLCJwY"
-# db_user = "stocks"
+db_password = "kVXBUbyI6IjIwMjAtMDgtMDIifSx7ImNvZGUiOiJQREIiLCJwY"
+db_user = "stocks"
 
 db_name = "stocks"
