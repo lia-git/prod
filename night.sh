74be8ea0eaa8c96375305cf687fd4fac3c390ebb
Lia Leo <lialeo@LiadeMacBook-Pro.local>
2020-07-07 18:12:26 +0800

    update

--- night.sh
+++ night.sh
@@ -1,11 +1,11 @@
 cd ~/workspace/prod/
 echo 'start'
-python3 theme_base.py
-echo 'theme_base done'
-python3 mapping.py
-echo 'mapping done'
-python3 stock_base.py
-echo 'stocks done'
+#python3 theme_base.py
+#echo 'theme_base done'
+#python3 mapping.py
+#echo 'mapping done'
+#python3 stock_base.py
+#echo 'stocks done'
 python3 update_tmp_degree.py
 echo 'update_tmp_degree done'
 python3 update_stock_history.py
