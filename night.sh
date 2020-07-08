d6386421c5b604f6b9f5fee8cffe05cad0b70f25
Lia Leo <lialeo@LiadeMacBook-Pro.local>
2020-07-08 21:09:43 +0800

    update

--- night.sh
+++ night.sh
@@ -2,12 +2,12 @@
 echo "start"
 . /etc/profile
 cd /home/ubuntu/workspace/prod
-#python3 update_tmp_degree.py
-#echo 'update_tmp_degree done'
-#python3 update_stock_history.py
-#echo 'update_stock_history done'
-#python3 update_theme_history.py
-#echo 'update_theme_history done'
+python3 update_tmp_degree.py
+echo 'update_tmp_degree done'
+python3 update_stock_history.py
+echo 'update_stock_history done'
+python3 update_theme_history.py
+echo 'update_theme_history done'
 python3 get_super_hot.py
 echo 'end'
 
