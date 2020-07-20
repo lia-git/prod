d2c9be83d52a606e5a9ced8d62872f60850c86bd
bowen <bowenliang@ddkt365.com>
2020-07-20 16:00:51 +0800

    update

--- every.py
+++ every.py
@@ -72,7 +72,7 @@ def get_select_theme_change():
                            database=setting.db_name, charset="utf8")  # 得到一个可以执行SQL语句的光标对象
     cursor = conn.cursor()
     try:
-        sql = f''' select th.theme_name,th.tmp_degree,th.bef_degree_1,th.bef_degree_2,th.bef_degree_3,tm.masters from theme_hot th join theme_stocks_map tm
+        sql = f''' select th.theme_name,th.tmp_degree,th.bef_degree_1,th.bef_degree_2,th.bef_degree_3,tm.stock_names from theme_hot th join theme_stocks_map tm
                 on th.theme_code = tm.theme_code  where th.theme_code not in ({','.join(setting.black_list)}) ;
                       '''
         cursor.execute(sql)
@@ -97,15 +97,38 @@ def get_select_theme_change():
     cursor.close()
     conn.close()
     limit_candits = set([can[0] for can in candits if can[1]>9.84])
-    high_candits = set([can[0] for can in candits if 1.8 <= can[1] <= 5])
+    second_candits = set([can[0] for can in candits if 5<=can[1] < 8])
+    high_candits = set([can[0] for can in candits if 1.8 <= can[1] < 5])
     low_candits = set([can[0] for can in candits if can[1]<1.8])
     ret = [[int(item[1].strip(",").split(",")[0]),
             item[:-1],
-            set(item[-1].split(".")) & limit_candits if item[-1] else set([]),
-            set(item[-1].split(".")) & high_candits if item[-1] else set([]),
-           set(item[-1].split(".")) & low_candits if item[-1] else set([])] for item in ret]
+            get_names_order(set(item[-1].split(".")) & limit_candits if item[-1] else set([])),
+            get_names_order(set(item[-1].split(".")) & second_candits if item[-1] else set([]))[:10],
+            get_names_order(set(item[-1].split(".")) & high_candits if item[-1] else set([]))[:10],
+           get_names_order(set(item[-1].split(".")) & low_candits if item[-1] else set([]))[:5]] for item in ret]
     final = sorted(ret,key=lambda i:i[0],reverse=True)
     return final
+
+def get_names_order(codes):
+    if not codes:
+        return codes
+    code_str  =",".join([f"'{code}'"  for code in codes])
+    conn = pymysql.connect(host="127.0.0.1", user=setting.db_user, password=setting.db_password,
+                           database=setting.db_name, charset="utf8")  # 得到一个可以执行SQL语句的光标对象
+    cursor = conn.cursor()
+    try:
+        # 执行SQL语句
+        cursor.execute(
+            f"select stock_name,change_pct from stock_base  where stock_code in ({code_str}) order by change_pct desc")
+        ret = cursor.fetchall()
+        # 提交事务
+        conn.commit()
+    except Exception as e:
+        # 有异常，回滚事务
+        traceback.print_exc()
+        conn.rollback()
+    return [f"{i[0]}/{i[1]}" for i in ret]
+
 
 
 def set_tmp_null():
@@ -126,57 +149,57 @@ def set_tmp_null():
 
 
 def to_file(res,name):
-    stocks = {}
-    with open("master.txt") as reader:
-        for line in reader:
-            code,name_ = line.strip().split("\t")
-            stocks[code.lower()] = name_
-    res_ = [[item[1][0],str(item[1][2:][::-1]),item[0],item[1][1],",".join([stocks[code] for code in item[2]]),",".join([stocks[code] for code in item[3]]),",".join([stocks[code] for code in item[4]])] for item in res]
-    df = pd.DataFrame(res_,columns=["版块","历史","最新","趋势","涨停","候选","低位"])
+    # stocks = {}
+    # with open("master.txt") as reader:
+    #     for line in reader:
+    #         code,name_ = line.strip().split("\t")
+    #         stocks[code.lower()] = name_
+    res_ = [[item[1][0],str(item[1][2:][::-1]),item[0],item[1][1],",".join(item[2]),",".join(item[3]),",".join( item[4]),",".join( item[5])] for item in res]
+    df = pd.DataFrame(res_,columns=["版块","历史","最新","趋势","涨停","高位","候选","低位"])
     df.to_excel(name)
     print()
 
 
-def update_mater_stocks():
-    stocks = {}
-    with open("master.txt") as reader:
-        for line in reader:
-            code,name = line.strip().split("\t")
-            stocks[code.lower()] = name
-
-    code_set,name_set = set(stocks.keys()),set(stocks.values())
-    conn = pymysql.connect(host="127.0.0.1", user=setting.db_user, password=setting.db_password,
-                           database=setting.db_name, charset="utf8")  # 得到一个可以执行SQL语句的光标对象
-    cursor = conn.cursor()
-    try:
-        sql = f''' select theme_code,stock_names from theme_stocks_map ;
-                      '''
-        cursor.execute(sql)
-        ret = cursor.fetchall()
-        conn.commit()
-    except Exception as e:
-        # 有异常，回滚事务
-        print(sql)
-        traceback.print_exc()
-        conn.rollback()
-    ret = [[ele[0],set(ele[1].split("."))] for ele in ret]
-
-    try:
-        for code,stocks in ret:
-            headers = stocks & code_set
-            if headers:
-                sql = f''' update theme_stocks_map set masters = '{".".join(headers)}' where theme_code = '{code}' ;
-                              '''
-                cursor.execute(sql)
-                conn.commit()
-    except Exception as e:
-        # 有异常，回滚事务
-        print(sql)
-        traceback.print_exc()
-        conn.rollback()
-    cursor.close()
-    conn.close()
-    # return final
+# def update_mater_stocks():
+#     stocks = {}
+#     with open("master.txt") as reader:
+#         for line in reader:
+#             code,name = line.strip().split("\t")
+#             stocks[code.lower()] = name
+#
+#     code_set,name_set = set(stocks.keys()),set(stocks.values())
+#     conn = pymysql.connect(host="127.0.0.1", user=setting.db_user, password=setting.db_password,
+#                            database=setting.db_name, charset="utf8")  # 得到一个可以执行SQL语句的光标对象
+#     cursor = conn.cursor()
+#     try:
+#         sql = f''' select theme_code,stock_names from theme_stocks_map ;
+#                       '''
+#         cursor.execute(sql)
+#         ret = cursor.fetchall()
+#         conn.commit()
+#     except Exception as e:
+#         # 有异常，回滚事务
+#         print(sql)
+#         traceback.print_exc()
+#         conn.rollback()
+#     ret = [[ele[0],set(ele[1].split("."))] for ele in ret]
+#
+#     try:
+#         for code,stocks in ret:
+#             headers = stocks & code_set
+#             if headers:
+#                 sql = f''' update theme_stocks_map set masters = '{".".join(headers)}' where theme_code = '{code}' ;
+#                               '''
+#                 cursor.execute(sql)
+#                 conn.commit()
+#     except Exception as e:
+#         # 有异常，回滚事务
+#         print(sql)
+#         traceback.print_exc()
+#         conn.rollback()
+#     cursor.close()
+#     conn.close()
+#     # return final
 
 
 
@@ -188,10 +211,10 @@ def main():
         hour, minute = time_now.hour, time_now.minute
         if hour == 8 and 40 < minute < 58:
             set_tmp_null()
-            update_mater_stocks()
+            # update_mater_stocks()
             wechat = WeChatPub()
             wechat.send_msg('开盘热度置空')
-        if hour in [10, 13, 14] or (hour == 11 and 0 <= minute <= 30) or (hour == 9 and minute > 4) or (hour in (21,) and minute < 10):
+        if hour in [10, 13, 14] or (hour == 11 and 0 <= minute <= 30) or (hour == 9 and minute > 4) or (hour in (16,) and minute < 10):
             update_stock_intime()
             get_tmp_theme_hot()
             file_name = str(time_now).replace("-","").replace(":","").replace(" ","")[:12]
