5594c30d338b285396dbc846c02c9d18add92893
bowen <bowenliang@ddkt365.com>
2020-07-28 10:35:08 +0800

    update

--- every.py
+++ every.py
@@ -172,7 +172,7 @@ def get_value(item):
     vls_ =[int(i) for i in item]
     vls_tmp = []
     for k in vls_:
-        if k == 0:
+        if k <= 1:
             break
         else:
             vls_tmp.append(k)
