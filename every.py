efb3c3dcae1994e6de7981158329476b23bf8158
bowen <bowenliang@ddkt365.com>
2020-07-30 19:46:25 +0800

    update

--- every.py
+++ every.py
@@ -219,7 +219,7 @@ def main():
             to_file(header_info, f"result/headers_{file_name}.xlsx",flag=False)
             wechat.send_file(f"result/headers_{file_name}.xlsx")
 
-        if hour in [10, 13, 14] or (hour == 11 and 0 <= minute <= 34) or (hour == 9 and minute >= 24) or (hour in (15,19) and minute < 40):
+        if hour in [10, 13, 14] or (hour == 11 and 0 <= minute <= 34) or (hour == 9 and minute >= 24) or (hour in (15,19) and minute < 49):
 
             update_stock_intime()
             t1 = time.time()
