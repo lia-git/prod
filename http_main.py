7ea3f4a023a2b23cc7501b8be4a235d609eb5506
Lia Leo <lialeo@LiadeMacBook-Pro.local>
2020-08-01 21:19:57 +0800

    update

--- http_main.py
+++ http_main.py
@@ -1,45 +1,28 @@
-# from flask import Flask, url_for, request, render_template
-from werobot import WeRoBot
+from flask import Flask, url_for, request, render_template
 
 from money_fluent import excute
-# from wechat_utl import WeChatPub
-# wechat = WeChatPub()
-# app = Flask(__name__)
-import werobot
+from wechat_utl import WeChatPub
 
-robot = WeRoBot(token='WElJQgbim')
+wechat = WeChatPub()
 
-@robot.handler
-def hello(message):
-    print(message)
-    return 'Hello World!'
+app = Flask(__name__)
 
-# 让服务器监听在 0.0.0.0:80
-robot.config['HOST'] = '0.0.0.0'
-robot.config['PORT'] = 80
-robot.run()
-# @app.route('/', methods=['POST', 'GET'])
-# def login():
-#     if request.method == 'GET':
-#         args = request.args
-#         code = args.get("code","")
-#         type = args.get("type","")
-#         # if request.form['user'] == 'admin':
-#         #     return 'Admin login successfully!'
-#         # else:
-#         try:
-#             excute(wechat,code,type)
-#         except:
-#             wechat.send_msg(f"http://18.163.236.133:10001/?code={code}&type={type}")
-#         return 'No such user!'\
-
-#
-#
-# @robot.handler
-# def robot():
-#     if request.method == 'GET':
-#         return "OK"
-#     # title = request.args.get('title', 'Default')
-#     # return render_template('login.html', title=title)
-# if __name__ == "__main__":
-#     app.run(host="0.0.0.0",port=80,debug=False)
+@app.route('/', methods=['POST', 'GET'])
+def login():
+    if request.method == 'GET':
+        print(request.url)
+        # args = request.args
+        # code = args.get("code","")
+        # type = args.get("type","")
+        # # if request.form['user'] == 'admin':
+        # #     return 'Admin login successfully!'
+        # # else:
+        # try:
+        #     excute(wechat,code,type)
+        # except:
+        #     wechat.send_msg(f"http://18.163.236.133:10001/?code={code}&type={type}")
+        return 'No such user!'
+    # title = request.args.get('title', 'Default')
+    # return render_template('login.html', title=title)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",port=80,debug=False)
