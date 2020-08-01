022ab9ec3af6cb9817cd3a367f73a2d1bf026089
Lia Leo <lialeo@LiadeMacBook-Pro.local>
2020-08-01 22:28:24 +0800

    update

--- http_main.py
+++ http_main.py
@@ -6,28 +6,41 @@ from flask import Flask, url_for, request, render_template
 # from wechat_utl import WeChatPub
 #
 # wechat = WeChatPub()
+from wx import WXBizMsgCrypt
 
 app = Flask(__name__)
 
 @app.route('/robot', methods=['POST', 'GET'])
 def login():
     if request.method == 'GET':
-        signature=request.args.get('signature')
+        signature=request.args.get('msg_signature')
         timestamp=request.args.get('timestamp')
         nonce=request.args.get('nonce')
-        token = "WElJQgbim"
-        list = [token, timestamp, nonce]
-        list.sort()
-        sha1 = hashlib.sha1()
-        sha1.update(list[0].encode('utf-8'))
-        sha1.update(list[1].encode('utf-8'))
-        sha1.update(list[2].encode('utf-8'))
-        hashcode = sha1.hexdigest()
-        echostr = request.args.get("echostr")
-        if hashcode == signature:
-            return echostr
-        else:
-            return ""
+        echostr=request.args.get('echostr')
+        token = "lPGx80LMeIvkQvS7oIqhPVJOt5FSuz"
+        sEncodingAESKey = "cErjBfNfr8hEAv2DpHmMDJPJvFmg2ESngAfvuIObmZf"
+        sCorpID = "ww49433899fdbb10f8"
+        wxcpt = WXBizMsgCrypt(token, sEncodingAESKey, sCorpID)
+        # sVerifyMsgSig=HttpUtils.ParseUrl("msg_signature")
+        # ret = wxcpt.VerifyAESKey()
+        # print ret
+        # sVerifyEchoStr = "fsi1xnbH4yQh0+PJxcOdhhK6TDXkjMyhEPA7xB2TGz6b+g7xyAbEkRxN/3cNXW9qdqjnoVzEtpbhnFyq6SVHyA=="
+        ret, sEchoStr = wxcpt.VerifyURL(signature, timestamp, nonce, echostr)
+        if (ret != 0):
+            print(sEchoStr)
+            return sEchoStr
+        # list = [token, timestamp, nonce]
+        # list.sort()
+        # sha1 = hashlib.sha1()
+        # sha1.update(list[0].encode('utf-8'))
+        # sha1.update(list[1].encode('utf-8'))
+        # sha1.update(list[2].encode('utf-8'))
+        # hashcode = sha1.hexdigest()
+        # echostr = request.args.get("echostr")
+        # if hashcode == signature:
+        #     return echostr
+        # else:
+        #     return ""
 
         # print(request.url)
         # print(request.get_data())
@@ -41,7 +54,7 @@ def login():
         #     excute(wechat,code,type)
         # except:
         #     wechat.send_msg(f"http://18.163.236.133:10001/?code={code}&type={type}")
-        return 'No such user!'
+        # return 'No such user!'
     # title = request.args.get('title', 'Default')
     # return render_template('login.html', title=title)
 if __name__ == "__main__":
