638b16fb2fc499ee884fb2e6d0ad0f4c1f78695c
bowen <bowenliang@ddkt365.com>
2020-08-10 12:22:58 +0800

    update

--- every.py
+++ every.py
@@ -321,8 +321,8 @@ def main():
                     wechat.send_remind("强提醒：10:00之前必须卖出")
 
                 update_stock_intime(file_name[-4:])
-                # t1 = time.time()
-                # wechat.send_msg(f"更新股价：{int(t1 -start)}s")
+                t1 = time.time()
+                wechat.send_msg(f"更新股价：{int(t1 -start)}s")
                 get_tmp_theme_hot()
                 # t2 = time.time()
                 # wechat.send_msg(f"更新日内临时热度：{int(t2 -t1)}s")
