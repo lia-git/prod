6c8407047b5c0aed6531d6a8aed8f8da7cd714d8
Lia Leo <lialeo@LiadeMacBook-Pro.local>
2020-08-16 20:48:23 +0800

    update

--- reply/pic_reply.py
+++ reply/pic_reply.py
@@ -30,7 +30,7 @@ def reply_today_main_power():
             .set_global_opts(title_opts=opts.TitleOpts(title=f"版块{name}趋势"),yaxis_opts=opts.AxisOpts(type_="value", min_=min(pcts.values()),max_=max(pcts.values()),axistick_opts=opts.AxisTickOpts(is_show=True),splitline_opts=opts.SplitLineOpts(is_show=True)))
     )
     line.render(path=f"templates/{key}{int(time.time())}.html")
-    content = {"code":f"整个{title}动向","desc":"关注主力走势","url":f"http://ec2-18-163-236-133.ap-east-1.compute.amazonaws.com/show/{change_key}{int(time.time())}"}
+    content = {"code":f"整个{title}动向","desc":"关注主力走势","url":f"http://ec2-18-163-236-133.ap-east-1.compute.amazonaws.com/show/{key}{int(time.time())}"}
     print(content)
     wechat = WeChatPub()
     wechat.send_markdown(content)
