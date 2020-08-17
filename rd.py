805f70905e57f96e287277e01b2bac48a3bd6865
bowen <bowenliang@ddkt365.com>
2020-08-17 21:46:06 +0800

    update

--- rd.py
+++ rd.py
@@ -1,15 +1,20 @@
 import json
 
 import redis
+import requests
 
 rdp = redis.ConnectionPool(host='localhost', port=6379)
 r = redis.StrictRedis(connection_pool=rdp, decode_responses=True)
 theme_list = r.keys("*")
 s = {}
-for theme_last in theme_list:
+for theme_last in theme_list[:10]:
     last_val = r.get(theme_last)
-    s[str(theme_last)] = str(last_val)
+    data = {"key": theme_list, "val": last_val}
+    headers = {
+        'Content-Type': 'application/json; charset=UTF-8',
+    }
+    resp = requests.post("http://120.79.164.150:8080/x", data=json.dumps(data), headers=headers)
 
-with open("rd.txt",mode="w+") as w:
-    json.dump(s,w,ensure_ascii=False)
+
+
 
