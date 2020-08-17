781edd0740994da3be2de85399fb7439f523a1d6
Lia Leo <lialeo@LiadeMacBook-Pro.local>
2020-08-18 00:38:12 +0800

    update

--- rd.py
+++ rd.py
@@ -5,18 +5,20 @@ import requests
 
 rdp = redis.ConnectionPool(host='localhost', port=6379)
 r = redis.StrictRedis(connection_pool=rdp, decode_responses=True)
-theme_list = r.keys("*")
-s = {}
-for theme_last in theme_list:
-    last_val = r.get(theme_last)
-    if "b'" not  in theme_last.decode():
-        print(theme_last.decode())
-        s[theme_last.decode()] = last_val.decode()
-    else:
-        print(theme_last.decode())
+# theme_list = r.keys("*")
+# s = {}
+# for theme_last in theme_list:
+#     last_val = r.get(theme_last)
+#     if "b'" not  in theme_last.decode():
+#         print(theme_last.decode())
+#         s[theme_last.decode()] = last_val.decode()
+#     else:
+#         print(theme_last.decode())
 
-with open("rdx.txt",mode="w+") as w:
-    json.dump(s,w,ensure_ascii=False)
+with open("rdx.txt",mode="r") as r_:
+    res = json.load(r_)
+    for k,v in res.items():
+        r.set(k,v)
     # data = {"key": theme_last.decode(), "val": last_val.decode()}
     # print(data)
     # headers = {
