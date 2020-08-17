ef31d811b4c6eaff1f1a7cc998564806b768f62c
Lia Leo <lialeo@LiadeMacBook-Pro.local>
2020-08-17 23:50:06 +0800

    update

--- rd.py
+++ rd.py
@@ -9,12 +9,19 @@ theme_list = r.keys("*")
 s = {}
 for theme_last in theme_list:
     last_val = r.get(theme_last)
-    data = {"key": theme_last.decode(), "val": last_val.decode()}
-    print(data)
-    headers = {
-        'Content-Type': 'application/json; charset=UTF-8',
-    }
-    resp = requests.post("http://120.79.164.150:8080/x", data=json.dumps(data), headers=headers)
+    if "b'" not  in theme_last.decode():
+        s[theme_last.decode()] = last_val.decode()
+    else:
+        print(theme_last.decode())
+
+with open("rdx.txt",mode="w+") as w:
+    json.dump(s,w,ensure_ascii=False)
+    # data = {"key": theme_last.decode(), "val": last_val.decode()}
+    # print(data)
+    # headers = {
+    #     'Content-Type': 'application/json; charset=UTF-8',
+    # }
+    # resp = requests.post("http://120.79.164.150:8080/x", data=json.dumps(data), headers=headers)
 
 
 
