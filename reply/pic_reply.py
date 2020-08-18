90c60b52d5a89d13dc45f8e9d71f76fbaf97612d
bowen <bowenliang@ddkt365.com>
2020-08-18 18:18:50 +0800

    update

--- reply/pic_reply.py
+++ reply/pic_reply.py
@@ -262,7 +262,7 @@ def get_dragon_code():
         traceback.print_exc()
         conn.rollback()
     # print(item,flush=True)
-    return items[:4]
+    return items
 
 def get_stock_code(name):
     conn = pymysql.connect(host="127.0.0.1", user=setting.db_user,password=setting.db_password,database=setting.db_name,charset="utf8")
