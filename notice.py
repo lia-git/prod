e7073b6dd23a38e847377ba16b57f0c6a01b0c49
bowen <bowenliang@ddkt365.com>
2020-08-28 15:19:10 +0800

    update

--- notice.py
+++ notice.py
@@ -8,6 +8,8 @@ import schedule
 from chinese_calendar import is_workday
 
 import setting
+from wechat_utl import WeChatPub_3 as WeChatPub
+
 
 def get_sina_info(code):
     url = f"http://hq.sinajs.cn/list={code}"
@@ -35,13 +37,18 @@ def update_notice_price():
         conn.rollback()
     # cursor.close()
     codes = []
-    print(items)
+    # print(items)
     for item in items:
          codes.append([item[0], json.loads(item[1]) if item[1] else []])
 
     for code,detail in codes:
         now_price = get_sina_info(code)
-        detail.append(now_price)
+        if detail:
+            if now_price != detail[-1]:
+                detail.append(now_price)
+        else:
+            detail.append(now_price)
+
         try:
             # 执行SQL语句
             cursor.execute(f"update price_notice set price_detail = '{json.dumps(detail)}' where stock_code = '{code}'")
@@ -53,6 +60,62 @@ def update_notice_price():
             conn.rollback()
 
 
+
+def notice():
+    conn = pymysql.connect(host="127.0.0.1", user=setting.db_user, password=setting.db_password,
+                           database=setting.db_name, charset="utf8")
+    # 得到一个可以执行SQL语句的光标对象
+    cursor = conn.cursor()
+
+    try:
+        # 执行SQL语句
+        cursor.execute(f"select  b.stock_name,n.buy_price,n.price_detail,n.flag,b.stock_code  from price_notice n join stock_base b on n.stock_code = b.stock_code")
+
+        items = cursor.fetchall()
+        # 提交事务
+        conn.commit()
+    except Exception as e:
+        # 有异常，回滚事务
+        traceback.print_exc()
+        conn.rollback()
+    records = []
+    for item in items:
+        records.append([item[0],item[1], json.loads(item[2]) if item[2] else [], json.loads(item[3]) if item[3] else {},item[4]])
+    wechat = WeChatPub()
+    for name,base_price,trend_price,flag,code in records:
+        now_price = trend_price[-1]
+        max_price = max(trend_price)
+        now_pct = round((now_price - base_price)/base_price,2)
+        if now_pct < -0.05:
+            wechat.send_msg(f"事件：{name}触及止损点\n价格：{now_price}")
+        for i in range(6,1,-1):
+            pct_bound =[0.06*i - 0.002 , 0.06*i+ 0.002]
+            price_bound = [base_price*(1+p) for p in pct_bound]
+            if price_bound[0] <= now_price < price_bound[1] and max_price > price_bound[1]:
+                if not flag.get(f"{i}th",False):
+                    wechat.send_msg(f"事件：{name} 触及第{i}止盈点\n价格：{now_price}")
+                    flag[f"{i}th"] = True
+                    update_flag(code,flag)
+
+
+
+
+def update_flag(code,flag):
+    conn = pymysql.connect(host="127.0.0.1", user=setting.db_user, password=setting.db_password,
+                           database=setting.db_name, charset="utf8")
+    # 得到一个可以执行SQL语句的光标对象
+    cursor = conn.cursor()
+    try:
+        # 执行SQL语句
+        cursor.execute(f"update price_notice set flag= '{json.dumps(flag)}' where stock_code = '{code}'")
+        # 提交事务
+        conn.commit()
+    except Exception as e:
+        # 有异常，回滚事务
+        traceback.print_exc()
+        conn.rollback()
+
+
 def main():
     today = datetime.date.today()
     if is_workday(today):
@@ -61,6 +124,7 @@ def main():
         hour, minute = time_now.hour, time_now.minute
         if hour in [10, 13, 14] or (hour == 11 and 0 <= minute <= 30) or (hour == 9 and minute >= 30):
             update_notice_price()
+            notice()
 
 
 if __name__ == '__main__':
--- setting.py
+++ setting.py
@@ -7,6 +7,7 @@ db_name = os.environ.get("SP_DB_NAME","")
 corp_id = os.environ.get("SP_WX_CORP_ID","")
 user_id = os.environ.get("SP_WX_USER_ID","")
 user_id_2 = os.environ.get("SP_WX_USER_ID_2","")
+user_id_3 = os.environ.get("SP_WX_USER_ID_3","")
 device_id = os.environ.get("DEVICE_ID","")
 # FFCAABAB-08AF-4412-AEF3-5D7DF3ECDEFC
 token = os.environ.get("TOKEN","")
