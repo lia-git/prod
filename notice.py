b559ea26981718e0b8241b3c27a6f85ef261af91
bowen <bowenliang@ddkt365.com>
2020-08-28 15:33:38 +0800

    update

--- notice.py
+++ notice.py
@@ -87,11 +87,11 @@ def notice():
     for name,base_price,trend_price,flag,code in records:
         now_price = trend_price[-1]
         max_price = max(trend_price)
-        now_pct = round((now_price - base_price)/base_price,2)
+        now_pct = round((now_price - base_price)/base_price,5)
         print(now_pct)
         if now_pct < -0.05:
             wechat.send_msg(f"事件：{name}触及止损点\n价格：{now_price}")
-        for i in range(6,1,-1):
+        for i in range(6,0,-1):
             print(i)
             pct_bound =[pixel*i - 0.002 , pixel*i+ 0.002]
             price_bound = [base_price*(1+p) for p in pct_bound]
