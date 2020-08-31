84efd5576f2fde9e38da05f272116472e836ca74
bowen <bowenliang@ddkt365.com>
2020-08-31 13:49:25 +0800

    update

--- stock_base.py
+++ stock_base.py
@@ -61,7 +61,7 @@ def get_all_db(flag=True):
     cursor = conn.cursor()
     try:
         # 执行SQL语句
-        cursor.execute(f"select  {segment}  from stock_base where stock_code not  like 'sz300%' and stock_name not like '%ST%' and last_price between 4.0 and 50 ;")
+        cursor.execute(f"select  {segment}  from stock_base where stock_code not  like 'sz300%' and stock_name not like '%ST%' and last_price between 4.0 and 500 ;")
 
         items = cursor.fetchall()
         # 提交事务
